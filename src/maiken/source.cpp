ae733db0c48dfdeea0352a2a1bea21e84281027a
dekken <philip.deegan@gmail.com>
2017-08-21 17:30:23 +0100

    regex for sources

--- src/maiken/source.cpp
+++ src/maiken/source.cpp
@@ -29,6 +29,109 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include "maiken.hpp"
+#include <regex>
+
+class Regexer{
+
+    friend class maiken::Application;
+    static std::vector<std::string>
+    RESOLVE_REGEX(std::string str) KTHROW(kul::Exception){
+        std::vector<std::string> v;
+        auto posL = str.find("(");
+        auto posR = str.find(")");
+        if(posL == std::string::npos || posR == std::string::npos) return v;
+
+        if(str.size() > 1 && str.substr(0, 2) == "./") str = str.substr(2);
+
+        kul::Dir d(str);
+        std::string built, prnt;
+        std::vector<std::string> bits;
+        bits.insert(bits.begin(), d.name());
+        while(d.parent().name() != prnt && !d.parent().root()){
+            bits.insert(bits.begin(), d.parent().name());
+            prnt = d.parent().name();
+            d = d.parent();
+        }
+        if(d.parent().root()) {
+            bits.insert(bits.begin(), d.parent().name());
+        }else{
+            d = kul::env::CWD();
+            bits.insert(bits.begin(), d.name());
+            while(d.parent().name() != prnt && !d.parent().root()){
+                bits.insert(bits.begin(), d.parent().name());
+                prnt = d.parent().name();
+                d = d.parent();
+            }
+            str = kul::env::CWD() + kul::Dir::SEP() + str;
+        }
+
+        std::string rem, rule;
+        size_t bitsIndex = 0;
+        for(const auto& s : bits){
+            auto posL = s.find("(");
+            auto posR = s.find(")");
+            if(posL != std::string::npos && posR != std::string::npos){
+                if(built.size() + s.size() + 2 > str.size()){
+                    rem = str.substr(built.size() + s.size() + 1);
+                }else
+                    rem = str.substr(built.size() + s.size() + 2);
+                rule = s;
+                break;
+            }
+            if(kul::Dir(s).root())     built = s;
+            else
+            if(kul::Dir(built).root()) built = built + s;
+            else                       built = built + kul::Dir::SEP() + s;
+            bitsIndex++;
+        }
+        d = built;
+
+        auto regexer = [&](auto items){
+            for(const auto& item : items){
+                try {
+                    std::regex re(rule);
+                    std::smatch match;
+                    std::string subject(item.name());
+                    if(std::regex_search(subject, match, re) && match.size() > 1)
+                        RESOLVE_REGEX_REC(item.real(), built, subject, rem, bits, bitsIndex, v);
+                } catch (std::regex_error& e) {
+                    KEXIT(1, "Regex Failure:\n") << e.what();
+                }
+            }
+        };
+        regexer(d.dirs());
+        regexer(d.files(0));
+        return v;
+    }
+
+    static
+    void
+    RESOLVE_REGEX_REC(
+            const std::string& i,
+            const std::string& b,
+            const std::string& s,
+            const std::string& r,
+            const std::vector<std::string>& bits,
+            const size_t& bitsIndex,
+            std::vector<std::string>& v) KTHROW(kul::Exception){
+
+        if(kul::File(i).is() && !kul::Dir(i).is()){
+            v.push_back(i);
+            return;
+        }
+
+        if(bits.size() >= bitsIndex + 1){
+            std::string n(kul::Dir::JOIN(b, kul::Dir::JOIN(s, r)));
+
+            const auto again = RESOLVE_REGEX(n);
+            if(again.empty()){
+                v.push_back(n);
+            }else
+                for(const auto& v1 : again) v.push_back(v1);
+        }
+    }
+
+};
 
 void
 maiken::Application::addSourceLine(const std::string& s) KTHROW(kul::Exception){
@@ -36,12 +139,19 @@ maiken::Application::addSourceLine(const std::string& s) KTHROW(kul::Exception){
     kul::String::TRIM(o);
     if(o.find(',') == std::string::npos){
         for(const auto& s : kul::cli::asArgs(o)){
-            kul::Dir d(Properties::RESOLVE(*this, s));
-            if(d) srcs.push_back(std::make_pair(d.real(), true));
+            std::string pResolved(Properties::RESOLVE(*this, s));
+            kul::Dir d(pResolved);
+            if(d.is()) srcs.push_back(std::make_pair(d.real(), true));
             else{
-                kul::File f(kul::Dir(Properties::RESOLVE(*this, s)).locl());
+                kul::File f(d.locl());
                 if(f) srcs.push_back(std::make_pair(f.real(), false));
-                else  KEXIT(1, "source does not exist\n"+s+"\n"+project().dir().path());
+                else {
+                    auto regexResolved(Regexer::RESOLVE_REGEX(pResolved));
+                    if(regexResolved.empty())
+                        KEXIT(1, "source does not exist\n"+s+"\n"+project().dir().path());
+                    for(const auto & item : regexResolved)
+                        srcs.push_back(std::make_pair(item, false));
+                }
             }
         }
     }else{
