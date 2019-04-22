bee316cbefc9de02af4919ee25bb31fe0431c4b2
dekken <philip.deegan@gmail.com>
2019-04-22 15:32:46 +0200

    gcc 8 on travis

--- res/ci/nix_travis.sh
+++ res/ci/nix_travis.sh
@@ -7,7 +7,7 @@ cd $CWD/../..
 ROOT=$PWD
 
 export MKN_GCC_PREFERRED=1
-sudo ln -s /usr/bin/gcc-7 /usr/local/bin/gcc; sudo ln -s /usr/bin/g++-7 /usr/local/bin/g++; gcc -v;
-make nix CXX=/usr/bin/g++-7
+sudo ln -s /usr/bin/gcc-8 /usr/local/bin/gcc; sudo ln -s /usr/bin/g++-8 /usr/local/bin/g++; gcc -v;
+make nix
 ./mkn build -dta "-std=c++14 -fPIC" -l "-pthread -ldl" -WOg 0 merge
 ./mkn build -dta "-std=c++14 -fPIC" -l "-pthread -ldl" -WOg 0 -p lib
