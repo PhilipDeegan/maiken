cff79cee74e6ed44e56727eed7a6860352fb750d
dekken <philip.deegan@gmail.com>
2020-05-08 14:00:44 +0200

    static CI binary

--- res/ci/nix_travis.sh
+++ res/ci/nix_travis.sh
@@ -9,4 +9,4 @@ ROOT=$PWD
 export MKN_GCC_PREFERRED=1
 make nix CXX="$CXX"
 CXXFLAGS="-std=c++17 -fPIC"
-KLOG=3 ./mkn build -WOdKta "$CXXFLAGS" -g 0
+KLOG=3 ./mkn build -WOdKta "$CXXFLAGS" -g 0 -p static
