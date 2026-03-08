4a4b3cbf871dbcea0ca95272162709eca8c6632a
PhilipDeegan <philip.deegan@gmail.com>
2026-03-08 19:02:42 +0100

    ++

--- src/maiken/mods.cpp
+++ src/maiken/mods.cpp
@@ -2,7 +2,7 @@
 Copyright (c) 2026, Philip Deegan.
 All rights reserved.
 
-Redistribution and use in source and binary forms, mod or modout
+Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are
 met:
 
@@ -10,7 +10,7 @@ met:
 notice, this list of conditions and the following disclaimer.
     * Redistributions in binary form must reproduce the above
 copyright notice, this list of conditions and the following disclaimer
-in the documentation and/or other materials provided mod the
+in the documentation and/or other materials provided with the
 distribution.
     * Neither the name of Philip Deegan nor the names of its
 contributors may be used to endorse or promote products derived from
