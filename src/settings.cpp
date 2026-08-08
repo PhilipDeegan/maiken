0ec3d85599542f882c1352db079d5ac965c4dea1
PhilipDeegan <philip.deegan@gmail.com>
2026-08-08 22:10:46 +0200

    accept absolute path of -x (#132)

--- src/settings.cpp
+++ src/settings.cpp
@@ -151,6 +151,15 @@ mkn::kul::File maiken::Settings::RESOLVE(std::string const& s, Settings const* s
 
   mkn::kul::Dir const cwd{mkn::kul::env::CWD()};
   auto file = [&]() -> std::optional<File_t> {
+    bool const is_abs = !s.empty() && (s.compare(0, mkn::kul::Dir::SEP().size(),
+                                                  mkn::kul::Dir::SEP()) == 0 ||
+                                        (s.size() > 1 && s[1] == ':'));
+    if (is_abs) {
+      if (auto f = File_t{s}; f) return f;
+      for (auto const& suffix : {".yml", ".yaml"})
+        if (auto f = File_t{s + suffix}; f) return f;
+      return std::nullopt;
+    }
     auto const dirs =
         settings ? std::vector{cwd, File_t{settings->file()}.dir(), mkn::kul::user::home("maiken")}
                  : std::vector{cwd, mkn::kul::user::home("maiken")};
