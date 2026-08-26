12d085a180dc117c0dd0f7c7b480992644a324f1
PhilipDeegan <philip.deegan@gmail.com>
2026-08-26 18:24:32 +0200

    forward builddir in context to modules

--- src/maiken/context.cpp
+++ src/maiken/context.cpp
@@ -45,6 +45,7 @@ maiken::ContextState maiken::ApplicationContext::state() const {
     for (auto const& byDir : byType.second)
       for (auto const& src : byDir.second) s.sourceFiles.push_back(src.in);
   s.compileEnv = CompileEnv{app.envVars(), app.compilationInfo()};
+  if (!s.sourceFiles.empty() || app.main()) s.extra["buildDir"] = app.buildDir().real();
   return s;
 }
 
